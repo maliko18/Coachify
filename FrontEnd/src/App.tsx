c88f8a31eaa80627053727347898cd507444f046
Zakariae ABDOUNI <abdouni.zakariae@gmail.com>
2026-01-23 17:57:31 +0100

    chore: initialisation du projet

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-5xl text font-bold text-blue-900">
+        Initialisation du projet
+      </h1>
+    </div>
+  );
+}
+
+export default App
+
